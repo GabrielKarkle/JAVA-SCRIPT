2580d5f58ba100b42b5f9763f0505b24fe3c9426
Gabriel Erich Karkle <gabrielkarkleoficial@gmail.com>
2025-09-13 15:17:35 -0300

    aprendendo if e else

---
+++ exercícios/base de Javascript/ex008.js
@@ -0,0 +1,8 @@
+var vel =  '59' 
+ console.log(`a sua velocidade atual e de ${vel}km/h`)
+if( vel > 60 ) {
+    console.log(`voce ultrapassou o radar,Multado `)
+}
+else{(vel < 60 ) 
+    console.log(`tu e uma lesma`)
+}
