73af6432922aaa1af0939d2893bd4fd36ba64ec9
Gabriel Erich Karkle <gabrielkarkleoficial@gmail.com>
2025-09-19 20:04:28 -0300

    terminando projeto basico pequeno selecionador de variaveis

--- exercícios/base de Javascript/ex0014 desafio seletor/script.js
+++ exercícios/base de Javascript/ex0014 desafio seletor/script.js
@@ -8,15 +8,44 @@ function carregar() {
     } else { 
         var fsex = document.getElementsByName('vtsex')
         idade = ano - Number(anoat.value)
-        res.innerHTML = (`idade calculada  ${idade} anos`)
-           var genero = ''
+        var genero = '' 
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
         if (fsex[0].checked) {
             genero = 'masculino'
+            if (idade >= 0 && idade < 10){
+             //bebe 
+             img.setAttribute('src', 'imagens/bebe-homem-m.jpg')
+            }else if (idade <= 21) {
+             //jovem
+             img.setAttribute('src', 'imagens/jovem-homem-m.jpg')
+            }else if ( idade <= 45) {
+             //adulto
+             img.setAttribute('src', 'imagens/adult-homem-m.jpg')
+            }else {
+            //vovo
+            img.setAttribute('src', 'imagens/vovo-homem-m.jpg')
+            } 
+            
         } else if (fsex[1].checked) {
             genero = 'feminino'
+            if (idade >= 0 && idade < 10){
+             //bebe 
+             img.setAttribute('src', 'imagens/bebe-mulher-f.jpg')
+            }else if (idade <= 21) {
+             //jovem
+             img.setAttribute('src', 'imagens/jovem-mulher-f.jpg')
+            }else if ( idade <= 45) {
+             //adulto
+             img.setAttribute ('src', 'imagens/adult-mulher-f.jpg')
+            }else {
+            //vovo
+            img.setAttribute('src', 'imagens/vovo-mulher-f.jpg')} 
+
         }
         res.style.textAlign = 'center'
         res.innerHTML = `sexo : ${genero} - idade : ${idade}`
+        res.appendChild(img)
         
      }
 }
