93d53595b758b03a6eb5fa0a315dfddadc85fcab
Gabriel Erich Karkle <gabrielkarkleoficial@gmail.com>
2025-09-20 14:46:59 -0300

    aprendendo a usar while do e for de forma parctica e facil usando exercicios

---
+++ exercícios/base de Javascript/exercicios/ex013.js
@@ -0,0 +1,12 @@
+
+var c = 1
+ do {console.log (`paso ${c}`) 
+ c++
+} while (c <= 5)
+
+/*var c = 0
+while (c <= 5) {
+    console.log(`paso ${c}`)
+    c++ 
+}
+*/
---
+++ exercícios/base de Javascript/exercicios/ex014.js
@@ -0,0 +1,19 @@
+
+console.log ('começar agora...')
+for(var c = 1 ; c <= 5 ; c++)
+    console.log(c)
+console.log('acabou')
+/*
+var c = 1 
+do {console.log (c) 
+    c++  
+} while(c <= 8)
+ */
+
+/*
+var c = 1 
+while (c <= 5 ){
+    console,console.log(c);
+    c++
+}
+*/
